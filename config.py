43aa274e697a6a4411c8e5b7170127c2fbd68027
Max00355 <max00355@gmail.com>
2013-12-10 14:52:35 -0500

    Fixed relay nodes

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import landerdb
 
 relay = 0
-brokers = [{"ip":"", "port":2525}]
+brokers = [{"ip":"zcoin.zapto.org", "port":2525}, {"ip":"192.111.130.31", "port":2525}]
 version = "0.1.0"
 host = ""
 port = 2525
