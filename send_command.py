bbdc27df1888e47ffedaa61934060fd700a00726
Max00355 <max00355@gmail.com>
2013-12-10 23:21:24 -0500

    Added timeout and fixed .recv() problem

--- send_command.py
+++ send_command.py
@@ -14,18 +14,23 @@ def send(cmd, out=False, god=False):
     for x in nodes:
         s = socket.socket()
         try:
+            s.settimeout(30)
             s.connect((x['ip'], x['port']))
         except:
             s.close()
             continue
         else:
             s.send(json.dumps({"cmd":"get_version"}))
-            data = s.recv(1024)
+            try:
+                data = s.recv(1024)
+            except:
+                s.close()
+                continue
             if data == config.version:
                 s.close()
                 s = socket.socket()
                 try:
-                    s.settimeout(60)
+                    s.settimeout(30)
                     s.connect((x['ip'], x['port']))
                 except:
                     s.close()
@@ -33,12 +38,19 @@ def send(cmd, out=False, god=False):
                 else:
                     s.send(json.dumps(cmd))
                     out = ""
+                    cont = False
                     while True:
-                        data = s.recv(1024)
+                        try:
+                            data = s.recv(1024)
+                        except:
+                            cont = True
+                            break
                         if not data:
                             break
                         out = out + data
                     s.close()
+                    if cont:
+                        continue
                     if out:
                         return out
             else:
