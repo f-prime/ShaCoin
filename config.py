ed6b1f90ffd016cf5b4ef5a52cdcc40588e568bd
Max00355 <max00355@gmail.com>
2013-12-21 11:56:31 -0500

    added other godnode

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import landerdb
 
 relay = 0
-brokers = [{"ip":"zcoin.zapto.org", "port":2525}]#[{"ip":"zcoin.zapto.org", "port":6564}, {"ip":"192.111.130.31", "port":6565}]
+brokers = [{"ip":"zcoin.zapto.org", "port":2525, "192.111.130.31":2525}]#[{"ip":"zcoin.zapto.org", "port":6564}, {"ip":"192.111.130.31", "port":6565}]
 version = "0.2.2"
 host = "0.0.0.0"
 port = 2525
